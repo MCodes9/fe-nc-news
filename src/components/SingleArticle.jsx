7cb2091d529a51abb893b83eb68775012d249133
Malavika Mysore <malavika.mysore@gmail.com>
2022-06-15 15:42:05 +0100

    Ticket 6: View single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { articleById } from "../utils/api";
 import formatDate from "../utils/formatDate";
+import "./SingleArticle.css";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
