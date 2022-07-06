a72e0228c1785bc9a1d7a2205447f451578a2317
Malavika <malavika.mysore@gmail.com>
2022-07-06 21:46:46 +0100

    remove redundant code

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,10 +5,10 @@ const ArticleCard = ({ article }) => {
   return (
     <li key={article.article_id} className="card">
       <Link to={`/articles/${article.article_id}`}>
-        <h3>{article.title}</h3>
+        <h2>{article.title}</h2>
       </Link>
-      <h4>Written by {article.author}</h4>
-      <h5> {article.topic}</h5>
+      <h3>Written by {article.author}</h3>
+      <h3> {article.topic}</h3>
       <p>Date: {formatDate(article.created_at)}</p>
       <p>{article.body}</p>
       <p>Votes: {article.votes}</p>
